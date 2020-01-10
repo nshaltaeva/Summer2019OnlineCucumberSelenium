49281d2e508b3858dce94e970969ab58493c8d5a
nshaltaeva <nshaltaeva@gmail.com>
2020-01-09 19:29:33 -0500

    jan_03_20

--- src/test/java/com/vytrack/runners/RegressionRunner.java
+++ src/test/java/com/vytrack/runners/RegressionRunner.java
@@ -1,22 +1,21 @@
 package com.vytrack.runners;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features", //path to features
-   //     features = "src/test/resources/features/activities/CalendarEvents.feature", //path to features
+        features = {"src/test/resources/features/activities", //path to features
+                    "src/test/resources/features/fleet"},
+        //     features = "src/test/resources/features/activities/CalendarEvents.feature", //path to features
         glue = "com/vytrack/step_definitions",    //path to step definitions
         dryRun = false, //by default it is false
-        tags = "@login_with_credentials_ddt",
         plugin = {"html:target/default-cucumber-reports",
-                    "json:target/cucumber.json",
+                "json:target/cucumber.json",
                 "rerun:target/rerun.txt"
         }
 )
-public class CucumberRunner {
 
+public class RegressionRunner {
 }
--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -1,59 +1,133 @@
 package com.vytrack.utilities;
 
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
-    private static WebDriver driver;
+//    private static WebDriver driver;
+//
+//    //if constructor is private, you cannot do like this Driver obj = new Driver()
+//    private Driver(){
+//
+//    }
+//
+//    public static WebDriver get(){
+//        //if webdriver object was not created yet
+//        if(driver == null){
+//            //create webdriver object based on browser value from properties file
+//            String browser = ConfigurationReader.getProperty("browser");
+//            switch(browser){
+//                case "chrome":
+//                    WebDriverManager.chromedriver().setup();
+//                //    ChromeOptions chromeOptions = new ChromeOptions();
+//                //    chromeOptions.setHeadless(false);
+//                //    driver = new ChromeDriver(chromeOptions);
+//                    driver = new ChromeDriver();
+//                    break;
+//                case "chrome_headless":
+//                    WebDriverManager.chromedriver().setup();
+//                    ChromeOptions chromeOptions = new ChromeOptions();
+//                    chromeOptions.setHeadless(true);
+//                    driver = new ChromeDriver(chromeOptions);
+//                    break;
+//                case "firefox":
+//                    WebDriverManager.firefoxdriver().setup();
+//                    driver = new FirefoxDriver();
+//                    break;
+//                default:
+//                    //if browser type  is wrong, throw exception
+//                    //no browser will be opened
+//                    throw new RuntimeException("Wrong browser type!");
+//            }
+//        }
+//
+//        return driver;
+//    }
+//
+//    public static void close(){
+//        //if driver still exist
+//        if(driver != null){
+//            //close all browsers
+//            driver.quit();
+//            //destroy driver object, ready for garbage collection
+//            driver = null;
+//        }
+//    }
 
-    //if constructor is private, you cannot do like this Driver obj = new Driver()
-    private Driver(){
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
+
+    private Driver() {
 
     }
 
-    public static WebDriver get(){
-        //if webdriver object was not created yet
-        if(driver == null){
-            //create webdriver object based on browser value from properties file
-            String browser = ConfigurationReader.getProperty("browser");
-            switch(browser){
+    public static WebDriver get() {
+        //if this thread doesn't have a web driver yet - create it and add to pool
+        if (driverPool.get() == null) {
+            System.out.println("TRYING TO CREATE DRIVER");
+            // this line will tell which browser should open based on the value from properties file
+            String browserParamFromEnv = System.getProperty("browser");
+            String browser = browserParamFromEnv == null ? ConfigurationReader.getProperty("browser") : browserParamFromEnv;
+            switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                //    ChromeOptions chromeOptions = new ChromeOptions();
-                //    chromeOptions.setHeadless(false);
-                //    driver = new ChromeDriver(chromeOptions);
-                    driver = new ChromeDriver();
+                    driverPool.set(new ChromeDriver());
                     break;
                 case "chrome_headless":
                     WebDriverManager.chromedriver().setup();
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    chromeOptions.setHeadless(true);
-                    driver = new ChromeDriver(chromeOptions);
+                    driverPool.set(new ChromeDriver(new ChromeOptions().setHeadless(true)));
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    driverPool.set(new FirefoxDriver());
                     break;
-                default:
-                    //if browser type  is wrong, throw exception
-                    //no browser will be opened
-                    throw new RuntimeException("Wrong browser type!");
+                case "firefox_headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver(new FirefoxOptions().setHeadless(true)));
+                    break;
+                case "ie":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your OS doesn't support Internet Explorer");
+                    }
+                    WebDriverManager.iedriver().setup();
+                    driverPool.set(new InternetExplorerDriver());
+                    break;
+                case "edge":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your OS doesn't support Edge");
+                    }
+                    WebDriverManager.edgedriver().setup();
+                    driverPool.set(new EdgeDriver());
+                    break;
+                case "safari":
+                    if (!System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your OS doesn't support Safari");
+                    }
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driverPool.set(new SafariDriver());
+                    break;
             }
-        }
 
-        return driver;
+        }
+        //return corresponded to thread id webdriver object
+        return driverPool.get();
     }
 
-    public static void close(){
-        //if driver still exist
-        if(driver != null){
-            //close all browsers
-            driver.quit();
-            //destroy driver object, ready for garbage collection
-            driver = null;
-        }
+    public static void close() {
+        driverPool.get().quit();
+        driverPool.remove();
     }
 }
